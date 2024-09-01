ae6043efdabb199b36bdb8253ad2b100288f82c9
Mikhail Prokhorov <mprohorov@evapps.ru>
2024-09-01 09:39:32 +0300

    add PostGeneratedSchemaListener

---
+++ src/Application/Doctrine/PostGenerateSchemaEventListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Application\Doctrine;
+
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
+use Doctrine\DBAL\Schema\SchemaException;
+use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
+use Doctrine\ORM\Tools\ToolEvents;
+
+ #[AsDoctrineListener(event: ToolEvents::postGenerateSchema, connection: 'default')]
+class PostGenerateSchemaEventListener
+{
+    /**
+     * @param GenerateSchemaEventArgs $args
+     *
+     * @return void
+     * @throws SchemaException
+     */
+    public function postGenerateSchema(GenerateSchemaEventArgs $args): void
+    {
+        $schema = $args->getSchema();
+        if (! $schema->hasNamespace('public')) {
+            $schema->createNamespace('public');
+        }
+    }
+}
