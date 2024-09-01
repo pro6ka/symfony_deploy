ee64abae088376bf0e95be4d9909f096c78559dd
Mikhail Prokhorov <mprohorov@evapps.ru>
2024-09-01 10:05:12 +0300

    User entity

---
+++ src/Domain/Entity/Useer.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Domain\Entity;
+
+
+use App\Domain\Entity\Contracts\EntityInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Table(name: 'user')]
+#[ORM\Entity]
+#[ORM\Index(name: 'user__email_unique', columns: ['email'])]
+#[ORM\Index(name: 'user__login_unique', columns: ['login'])]
+class User implements EntityInterface
+{
+    #[ORM\Column(name: 'id', type: 'bigint', unique: true)]
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    private ?int $id;
+
+    #[ORM\Column(type: 'string', length: 32, unique: true, nullable: false)]
+    private string $login;
+
+    #[ORM\Column(type: 'string', length: 32, nullable: false)]
+    private string $firstName;
+
+    #[ORM\Column(type: 'string', length: 32, nullable: false)]
+    private string $lastName;
+
+    #[ORM\Column(type: 'string', length: 32, nullable: false)]
+    private string $middleName;
+
+    #[ORM\Column(type: 'string', length: 100, unique: true, nullable: false)]
+    private string $email;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return void
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param string $login
+     *
+     * @return void
+     */
+    public function setLogin(string $login): void
+    {
+        $this->login = $login;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstname(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     *
+     * @return void
+     */
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     *
+     * @return void
+     */
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMiddleName(): string
+    {
+        return $this->middleName;
+    }
+
+    /**
+     * @param string $middleName
+     *
+     * @return void
+     */
+    public function setMiddleName(string $middleName): void
+    {
+        $this->middleName = $middleName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     *
+     * @return void
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+}
