52ef067502c8d58f97c8ab23995bddddee2e0837
Mikhail Prokhorov <mprohorov@evapps.ru>
2024-09-20 05:35:19 +0300

    get revisions repository methods

--- src/Infrastructure/Repository/RevisionRepository.php
+++ src/Infrastructure/Repository/RevisionRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Infrastructure\Repository;
 
+use App\Domain\Entity\Contracts\RevisionableInterface;
 use App\Domain\Entity\Revision;
 use App\Infrastructure\Repository\AbstractRepository;
 
@@ -15,5 +16,43 @@ class RevisionRepository extends AbstractRepository
     public function create(Revision $revision): int
     {
         return $this->store($revision);
+    }
+
+    /**
+     * @param Revision $entity
+     *
+     * @return null|Revision
+     */
+    public function forEntityByEntityId(RevisionableInterface $entity): ?Revision
+    {
+        $queryBuilder = $this->entityManager->createQueryBuilder();
+        $queryBuilder->select('r')
+            ->from(Revision::class, 'r')
+            ->andWhere($queryBuilder->expr()->eq('r.entityId', ':entityId'))
+            ->andWhere($queryBuilder->expr()->eq('r.entityType', ':entityType'))
+            ->setParameter('entityId', $entity->getId())
+            ->setParameter('entityType', $entity::class);
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * @param RevisionableInterface $entity
+     *
+     * @return null|Revision
+     */
+    public function findLastForEntity(RevisionableInterface $entity): ?Revision
+    {
+        $queryBuilder = $this->entityManager->createQueryBuilder();
+        $queryBuilder->select('r')
+            ->from(Revision::class, 'r')
+            ->andWhere($queryBuilder->expr()->eq('r.entityId', ':entityId'))
+            ->andWhere($queryBuilder->expr()->eq('r.entityType', ':entityType'))
+            ->setParameter('entityId', $entity->getId())
+            ->setParameter('entityType', $entity::class)
+            ->orderBy('r.createdAt', 'DESC')
+            ->setMaxResults(1);
+
+        return $queryBuilder->getQuery()->getSingleResult();
     }
 }
