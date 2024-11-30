40c86e7ff325f3a06a0018e8c17b225902780148
Mikhail Prokhorov <mprohorov@evapps.ru>
2024-11-30 10:09:04 +0300

    test ci

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
   sudo service nginx restart
   sudo -u www-data composer install -q
   sudo service php8.3-fpm restart
-  sudo -u www-data sed -i -- "s|%DATABASE_HOST%|$2|g" .env
-  sudo -u www-data sed -i -- "s|%DATABASE_USER%|$3|g" .env
-  sudo -u www-data sed -i -- "s|%DATABASE_PASSWORD%|$4|g" .env
-  sudo -u www-data sed -i -- "s|%DATABASE_NAME%|$5|g" .env
+  # sudo -u www-data sed -i -- "s|%DATABASE_HOST%|$1|g" .env
+  # sudo -u www-data sed -i -- "s|%DATABASE_USER%|$2|g" .env
+  # sudo -u www-data sed -i -- "s|%DATABASE_PASSWORD%|$3|g" .env
+  # sudo -u www-data sed -i -- "s|%DATABASE_NAME%|$4|g" .env
   sudo -u www-data php bin/console doctrine:migrations:migrate --no-interaction
-  sudo -u www-data sed -i -- "s|%RABBITMQ_HOST%|$6|g" .env
-  sudo -u www-data sed -i -- "s|%RABBITMQ_USER%|$7|g" .env
-  sudo -u www-data sed -i -- "s|%RABBITMQ_PASSWORD%|$8|g" .env
+  # sudo -u www-data sed -i -- "s|%RABBITMQ_HOST%|$5|g" .env
+  # sudo -u www-data sed -i -- "s|%RABBITMQ_USER%|$6|g" .env
+  # sudo -u www-data sed -i -- "s|%RABBITMQ_PASSWORD%|$7|g" .env
   sudo service supervisor restart
